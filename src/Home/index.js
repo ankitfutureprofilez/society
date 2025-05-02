803b4c1d03f21f5f20af6c6a0ad79a2cd8af6648
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2025-05-02 14:22:38 +0530

    Ghp Backedn

--- src/Home/index.js
+++ src/Home/index.js
@@ -8,6 +8,7 @@ import Stakeholder from "./Stakeholder";
 import Ready from "./Ready";
 import Faq from "../component/Faq";
 import Contact from "./Contact";
+import MasonryLayout from "./MasonryLayout";
 
 
 function Home() {
